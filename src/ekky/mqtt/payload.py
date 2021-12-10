da0136dfde2a4af73ed453d116a9a70b255b29c5
ekharisma <ekharisma@outlook.com>
2021-12-10 14:44:37 +0700

    rewrite

--- src/ekky/mqtt/payload.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def get_time():
-    return datetime.now().isoformat()
-
-def get_temperature_value(index:int):
-    temperature_list = [i for i in range(25, 35)]
-    return temperature_list[index]
