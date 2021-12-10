c636c589ec87f6f90dda5a41397d62c9ea3b5801
ekharisma <ekharisma@outlook.com>
2021-12-10 12:29:37 +0700

    encode payload with protobuf

--- src/ekky/mqtt/publish.py
+++ src/ekky/mqtt/publish.py
@@ -1,15 +1,28 @@
-from paho.mqtt import client
+from time import sleep, time
+from src.ekky.mqtt import payload
+import paho.mqtt.client as mqtt
+import json
 
 class Payload:
     def __init__(self, id:int, temperature:int, timestamp:str) -> None:
-        self.id = 0
+        self.id = id
         self.temperature = temperature
         self.timestamp = timestamp
         self.port = 1883
         self.topic = "challenge/payload"
+        self.broker = "broker.hivemq.com"
 
-    def to_protobuf():
-        pass
+    def to_protobuf(self):
+        payload = {
+            'id': self.id,
+            'temperature': self.temperature,
+            'timestamp': self.timestamp
+        }
+        return json.dumps(payload)
 
-    def publish():
-        pass
+    def publish(self):
+        client = mqtt.Client("Py1")
+        print("Connecting to broker ", self.broker)
+        client.connect(self.broker)
+        sleep(4)
+        client.disconnect()
